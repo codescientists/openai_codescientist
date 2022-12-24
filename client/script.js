f60085df148c1c79a0b2bc96c1e19f581d7dace0
Piyush <piyushpp2412@gmail.com>
2022-12-24 15:23:38 +0530

    changed server url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-codescientist.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
